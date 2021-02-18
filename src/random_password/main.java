25f7f7b73abef1d709b41bcdf4dad796fb007c5e
Het Patel <hpp65933@uga.edu>
2021-02-18 18:58:01 -0500

    working file

--- src/random_password/main.java
+++ src/random_password/main.java
@@ -2,11 +2,11 @@ package random_password;
 
 import java.io.File;
 import java.util.Scanner;
+import Java.util.Random;
 
 public class main() {
 
     public static void main(string[] args) {
 
     }
-
 }
---
+++ src/random_password/randomPassword.java
@@ -0,0 +1,8 @@
+package random_password;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class randomPassword {
+    char[] ascii = {'a','b','c'};
+}
