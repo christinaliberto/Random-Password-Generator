0543acaf1888d9d5793b263b3fb45bc4faf1b16d
Christina Marie Liberto <cml53035@csci-odin.cs.uga.edu>
2021-02-18 19:03:26 -0500

    updated random generator

--- src/random_password/randomPassword.java
+++ src/random_password/randomPassword.java
@@ -4,5 +4,19 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class randomPassword {
-    char[] ascii = {'a','b','c'};
+
+    int randomNumber = (int)(Math.random() * 62);
+
+    if (randomNumber >= 0 || randomNumber <=9) {
+	randomNumber = randomNumber + 48;
+    }
+
+    if (randomNumber >= 10 || randomNumber <= 35) {
+	randomNumber = randomNumber + 55;
+    }
+
+    if (randomNumber >= 36 || randomNumber <= 61) {
+	randomNumber = randomNumber + 61;
+    }
+    
 }
